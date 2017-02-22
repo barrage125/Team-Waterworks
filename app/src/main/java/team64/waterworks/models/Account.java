763fa617b2ab45130ae6328694e4315b59857a4c
Anna Klaussen <anna.klaussen@gmail.com>
2017-02-22 18:24:52 -0500

    Add Profile field and dummy class

--- app/src/main/java/team64/waterworks/models/Account.java
+++ app/src/main/java/team64/waterworks/models/Account.java
@@ -2,15 +2,26 @@ package team64.waterworks.models;
 
 /**
  * Created by michal on 2/13/17.
+ *
+ * Edited by anna on 2/20/2017.
  */
 
 abstract class Account {
     private String name, username, password;
+    private Profile profile;
 
     Account(String name, String username, String pw) {
         this.name = name;
         this.username = username;
         this.password = pw;
+    }
+
+    public Profile getProfile() {
+        return profile;
+    }
+
+    public void setProfile(Profile profile) {
+        this.profile = profile;
     }
 
     public String getName() {
---
+++ app/src/main/java/team64/waterworks/models/Profile.java
@@ -0,0 +1,10 @@
+package team64.waterworks.models;
+
+/**
+ * anna
+ * 2/22/17
+ */
+
+public class Profile {
+    //dummy class
+}
