c53de097268873dc38204c8b4af5e6839bf4f272
Lotz, Alexander Michael <alexlotz@gatech.edu>
2017-03-03 20:54:05 -0500

    Completed Water Report Activity; Button additions on HomeActivity required for testing

--- app/src/main/java/team64/waterworks/controllers/WaterReportActivity.java
+++ app/src/main/java/team64/waterworks/controllers/WaterReportActivity.java
@@ -3,18 +3,24 @@ package team64.waterworks.controllers;
 import android.content.Intent;
 import android.location.Location;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.sql.Time;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 import team64.waterworks.R;
 import team64.waterworks.models.Account;
 import team64.waterworks.models.AccountsManager;
 import team64.waterworks.models.Profile;
+import team64.waterworks.models.ReportsManager;
+import team64.waterworks.models.WaterReport;
 
 /**
  * Created by Alexander on 3/3/2017.
@@ -22,9 +28,14 @@ import team64.waterworks.models.Profile;
 
 public class WaterReportActivity extends AppCompatActivity {
 
-    /*Button done;
-    EditText txtBDay, txtAddress, txtEmail, txtTitle;
+    Button submit, cancel;
+    EditText txtLong, txtLat, txtType, txtCondition;
+
     private Account account;
+    private Location location;
+    private String author;
+    private String type;
+    private String condition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,50 +43,43 @@ public class WaterReportActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
         account = AccountsManager.getActiveAccount();
 
-        done = (Button) findViewById(R.id.profDoneBTN);
-        txtAddress = (EditText) findViewById(R.id.profAddress);
-        txtBDay = (EditText) findViewById(R.id.profBDay);
-        txtEmail = (EditText) findViewById(R.id.profEmail);
-        txtTitle = (EditText) findViewById(R.id.profTitle);
-        
-        private Date date;
-        private int id;
-        private Location location;
-        private String author;
-        private String type;
-        private String condition;
+        submit = (Button) findViewById(R.id.profDoneBTN);
+        cancel = (Button) findViewById(R.id.profDoneBTN);
+        txtLong = (EditText) findViewById(R.id.wrLong);
+        txtLat = (EditText) findViewById(R.id.wrLat);
+        txtType = (EditText) findViewById(R.id.wrType);
+        txtCondition = (EditText) findViewById(R.id.wrCond);
 
-        if (account.getProfile() != null) {
-            Profile profile = account.getProfile();
-            txtAddress.setText(profile.getAddress());
-            txtBDay.setText(profile.getBirthday());
-            txtEmail.setText(profile.getEmail());
-            txtTitle.setText(profile.getTitle());
-        }
 
-        *//** Button handler for returning to home from ProfileActivity page*//*
-        done.setOnClickListener(new View.OnClickListener() {
+        //** Button handler for returning to home from WaterReportActivity page*//*
+        submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                account.getProfile().setAddress(txtAddress.getText().toString());
-                account.getProfile().setBirthday(txtBDay.getText().toString());
-                account.getProfile().setEmail(txtEmail.getText().toString());
-                account.getProfile().setTitle(txtTitle.getText().toString());
-
-                Profile new_profile = account.createProfile(txtTitle.getText().toString(),
-                        txtAddress.getText().toString(),
-                        txtEmail.getText().toString(),
-                        txtBDay.getText().toString());
-                if (AccountsManager.updateAccount(account.getUsername(), new_profile)) {
-                    Intent intent = new Intent(ProfileActivity.this, HomeActivity.class);
+                condition = txtCondition.getText().toString();
+                type = txtType.getText().toString();
+                author = account.getUsername();
+                location = new Location("");
+                location.setLongitude(Double.parseDouble(txtLong.getText().toString()));
+                location.setLatitude(Double.parseDouble(txtLat.getText().toString()));
+                if (ReportsManager.newReport(location, author, type, condition)) {
+                    Intent intent = new Intent(WaterReportActivity.this, HomeActivity.class);
                     startActivity(intent);
-                    Toast.makeText(getApplicationContext(), "Profile Saved", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "Report submitted", Toast.LENGTH_SHORT).show();
                 } else {
-                    Toast.makeText(getApplicationContext(), "Unable to save profile", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "Unable to submit", Toast.LENGTH_SHORT).show();
                 }
 
             }
         });
-    }*/
+
+        //** Button handler for returning to home from WaterReportActivity page w/o saving report*//*
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(WaterReportActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
 
