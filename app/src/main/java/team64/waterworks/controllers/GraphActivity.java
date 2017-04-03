4cb8a2ce56fa1498000fce61bbb09e2289f071ec
oliviamurkcaya <omurkcaya@gmail.com>
2017-04-02 23:53:04 -0400

    Added GraphView to gradle.build

---
+++ app/src/main/java/team64/waterworks/controllers/GraphActivity.java
@@ -0,0 +1,14 @@
+package team64.waterworks.controllers;
+
+
+public class GraphActivity {
+    GraphView graph = (GraphView) findViewById(R.id.graph);
+    LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+            new DataPoint(0, 1),
+            new DataPoint(1, 5),
+            new DataPoint(2, 3),
+            new DataPoint(3, 2),
+            new DataPoint(4, 6)
+    });
+graph.addSeries(series);
+}
