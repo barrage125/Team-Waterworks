fab8cd9fc80f0dbc7b274ba5337b6e7aa51dcf58
Michal <michalannamarble@gmail.com>
2017-04-03 00:17:34 -0400

    Fixed gradle issues

--- app/src/main/java/team64/waterworks/controllers/GraphActivity.java
+++ app/src/main/java/team64/waterworks/controllers/GraphActivity.java
@@ -1,14 +1,36 @@
 package team64.waterworks.controllers;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
-public class GraphActivity {
-    GraphView graph = (GraphView) findViewById(R.id.graph);
-    LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
-            new DataPoint(0, 1),
-            new DataPoint(1, 5),
-            new DataPoint(2, 3),
-            new DataPoint(3, 2),
-            new DataPoint(4, 6)
-    });
-graph.addSeries(series);
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+import team64.waterworks.R;
+
+public class GraphActivity extends AppCompatActivity implements View.OnClickListener {
+
+    /**
+     * Initializes all variables needed for graph activity
+     * @param savedInstanceState data passed into graph activity
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[]{
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
 }
