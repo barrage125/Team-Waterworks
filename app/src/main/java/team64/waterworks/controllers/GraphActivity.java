2081905b762fc53efd4267663175a1ca614a3210
Lotz, Alexander Michael <alexlotz@gatech.edu>
2017-04-03 07:31:21 -0400

    Added functionality for specifying graph criteria; Further implementation of graph required to completely test

--- app/src/main/java/team64/waterworks/controllers/GraphActivity.java
+++ app/src/main/java/team64/waterworks/controllers/GraphActivity.java
@@ -1,12 +1,20 @@
 package team64.waterworks.controllers;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.util.Date;
+
 import team64.waterworks.R;
 
 public class GraphActivity extends AppCompatActivity implements View.OnClickListener {
@@ -15,6 +23,12 @@ public class GraphActivity extends AppCompatActivity implements View.OnClickList
      * Initializes all variables needed for graph activity
      * @param savedInstanceState data passed into graph activity
      */
+    Double filterLong;
+    Double filterLat;
+    int filterRadius;
+    String filterStartDate;
+    String filterEndDate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +45,36 @@ public class GraphActivity extends AppCompatActivity implements View.OnClickList
 
     @Override
     public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.filter_graph_btn:
+            View view = (LayoutInflater.from(GraphActivity.this)).inflate(R.layout.dialog_graph_filter, null);
 
+            AlertDialog.Builder alertBuilder = new AlertDialog.Builder(GraphActivity.this);
+            alertBuilder.setView(view);
+
+            final EditText startDate = (EditText) view.findViewById(R.id.editText_Start_Date);
+            final EditText endDate = (EditText) view.findViewById(R.id.editText_End_Date);
+            final EditText longitude = (EditText) view.findViewById(R.id.editText_Longitude);
+            final EditText latitude = (EditText) view.findViewById(R.id.editText_Latitude);
+            final EditText radius = (EditText) view.findViewById(R.id.editText_Radius);
+
+            alertBuilder.setCancelable(true);
+            alertBuilder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    filterLong = Double.parseDouble(longitude.getText().toString());
+                    filterLat = Double.parseDouble(latitude.getText().toString());
+                    filterRadius = Integer.parseInt(radius.getText().toString());
+                    filterStartDate = startDate.getText().toString();
+                    filterEndDate = endDate.getText().toString();
+                }
+            });
+            alertBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    //Cancelled
+                }
+            });
+            Dialog dialog = alertBuilder.create();
+            dialog.show();
+        }
     }
 }
--- app/src/main/java/team64/waterworks/controllers/RegisterActivity.java
+++ app/src/main/java/team64/waterworks/controllers/RegisterActivity.java
@@ -1,10 +1,14 @@
 package team64.waterworks.controllers;
+import android.app.Dialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
