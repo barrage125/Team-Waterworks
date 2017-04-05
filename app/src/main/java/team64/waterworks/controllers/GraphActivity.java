3437a0da863d912bda381ac5819f774fd48cb868
oliviamurkcaya <omurkcaya@gmail.com>
2017-04-05 13:37:42 -0400

    testing if gitignore ignores /lib where manually imported jar file lives for GraphView

--- app/src/main/java/team64/waterworks/controllers/GraphActivity.java
+++ app/src/main/java/team64/waterworks/controllers/GraphActivity.java
@@ -2,6 +2,7 @@ package team64.waterworks.controllers;
 
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -10,12 +11,18 @@ import android.view.View;
 import android.widget.EditText;
 
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import team64.waterworks.R;
+import team64.waterworks.models.WPRManager;
+import team64.waterworks.models.WaterPurityReport;
 
 public class GraphActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -32,15 +39,141 @@ public class GraphActivity extends AppCompatActivity implements View.OnClickList
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_purity_graph_report);
         GraphView graph = (GraphView) findViewById(R.id.graph);
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[]{
-                new DataPoint(0, 1),
-                new DataPoint(1, 5),
-                new DataPoint(2, 3),
-                new DataPoint(3, 2),
-                new DataPoint(4, 6)
-        });
+        LineGraphSeries<DataPoint> virusSeries = null;
+        LineGraphSeries<DataPoint> contamSeries = null;
+
+        Date min = null;
+        Date max = null;
+
+        if (filterLat == null && filterLat == null) {
+            ArrayList<String> purityReports = WPRManager.viewAllPurityReports();
+            DataPoint[] virusDataPoints = new DataPoint[purityReports.size()];
+            DataPoint[] contamDataPoints = new DataPoint[purityReports.size()];
+            if (purityReports != null) {
+                for (int i = 0; i < purityReports.size(); i++) {
+                    String[] reportsData = purityReports.get(i).split(" ", 0);
+                    String idString = reportsData[0].replace("(", "").replace(")", "");
+                    Long idLong = Long.parseLong(idString);
+                    double virusPPM = (double) WPRManager.getPurityReportByID(idLong).getVirusPPM();
+                    double contamPPM = (double) WPRManager.getPurityReportByID(idLong).getContamPPM();
+                    String[] dateInString = WPRManager.getPurityReportByID(idLong).getDate().split(" ");
+                    System.out.println(dateInString[0]);
+                    SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+                    Date date = null;
+
+                    try {
+
+                        date = formatter.parse(dateInString[0]);
+                        if (min == null) {
+                            min = date;
+                            max = date;
+                        }
+
+                        if (min.compareTo(date) < 0) {
+                            min = date;
+                        }
+
+                        if (max.compareTo(date) > 0) {
+                            max = date;
+                        }
+
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+
+                    if (date != null) {
+                        virusDataPoints[i] = new DataPoint(date, virusPPM);
+                        contamDataPoints[i] = new DataPoint(date, contamPPM);
+                    }
+                }
+            }
+            if (virusDataPoints != null) {
+                virusSeries = new LineGraphSeries<>(virusDataPoints);
+            }
+
+            if (contamDataPoints != null) {
+                contamSeries = new LineGraphSeries<>(contamDataPoints);
+            }
+        } else {
+            ArrayList<WaterPurityReport> purityReports = WPRManager.getPurityReportsByLocationAndDate(filterLat, filterLong, filterStartDate, filterEndDate);
+            DataPoint[] virusDataPoints = null;
+            DataPoint[] contamDataPoints = null;
+            if (purityReports != null) {
+                contamDataPoints = new DataPoint[purityReports.size()];
+                virusDataPoints = new DataPoint[purityReports.size()];
+                for (int i = 0; i < purityReports.size(); i++) {
+                    double virusPPM = (double) purityReports.get(i).getVirusPPM();
+                    double contamPPM = (double) purityReports.get(i).getContamPPM();
+                    String dateInString = purityReports.get(i).getDate();
+
+                    SimpleDateFormat formatter = new SimpleDateFormat("(dd/MM/yyyy");
+                    Date date = null;
+
+                    try {
+
+                        date = formatter.parse(dateInString);
+
+                        if (min.compareTo(date) < 0) {
+                            min = date;
+                        }
+
+                        if (max.compareTo(date) > 0) {
+                            max = date;
+                        }
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+
+                    if (date != null) {
+                        virusDataPoints[i] = new DataPoint(date, virusPPM);
+                        contamDataPoints[i] = new DataPoint(date, contamPPM);
+                    }
+                }
+            }
+            if (virusDataPoints != null) {
+                virusSeries = new LineGraphSeries<>(virusDataPoints);
+            }
+
+            if (contamDataPoints != null) {
+                contamSeries = new LineGraphSeries<>(contamDataPoints);
+            }
+        }
+
+        if (graph != null && virusSeries != null) {
+            graph.addSeries(virusSeries);
+        }
+
+        if (graph != null && contamSeries != null) {
+            graph.getSecondScale().addSeries(contamSeries);
+        }
+
+        if (graph != null && virusSeries == null && contamSeries == null) {
+            graph.addSeries(new LineGraphSeries<>());
+        }
+
+        graph.getSecondScale().setMinY(0);
+        graph.getSecondScale().setMaxY(100);
+        contamSeries.setColor(Color.RED);
+
+        graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(getApplicationContext()));
+        graph.getGridLabelRenderer().setNumHorizontalLabels(3);
+
+        graph.getViewport().setMinX(min.getTime());
+        graph.getViewport().setMaxX(max.getTime());
+        graph.getViewport().setXAxisBoundsManual(true);
+
+        graph.getGridLabelRenderer().setHumanRounding(false);
+
+        graph.getViewport().setScalable(true); // enables horizontal zooming and scrolling
+        graph.getViewport().setScalableY(true); // enables vertical zooming and scrolling
+
+        virusSeries.setDrawDataPoints(true);
+        virusSeries.setDataPointsRadius(10);
+
+        contamSeries.setDrawDataPoints(true);
+        contamSeries.setDataPointsRadius(10);
     }
 
     @Override
